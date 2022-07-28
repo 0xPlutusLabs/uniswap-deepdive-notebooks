2b678f674f6578efdaff10b299498071a5624fd3
chinyasuhail <suhail@0xplutus.com>
2022-07-29 03:47:33 +0530

    minor changes

--- commonLPs_from_all_networks.py
+++ commonLPs_from_all_networks.py
@@ -6,6 +6,6 @@ v3_op = pd.read_csv("data/wallets/v3_op.csv")
 v3_polygon = pd.read_csv("data/wallets/v3_polygon.csv")
 
 output = v2_mainnet.merge(v3_mainnet, on="wallet_addr", how="inner")
-output = v3_mainnet.merge(v3_op, on="wallet_addr", how="inner")
+output = output.merge(v3_op, on="wallet_addr", how="inner")
 output = output.merge(v3_polygon, on="wallet_addr", how="inner")
 output.to_csv("commonLPs_from_all_networks.csv")
